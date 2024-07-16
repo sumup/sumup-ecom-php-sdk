3f06ffc9463b7c66e7ad412519ff07de74fb6cfe
Damien TUPINIER <damien.tupinier@gmail.com>
2024-07-16 12:33:33 +0200

    Change to Checkout creation: replace pay_to_email with the use of merchant_code

--- src/SumUp/Services/Checkouts.php
+++ src/SumUp/Services/Checkouts.php
@@ -61,7 +61,7 @@ class Checkouts implements SumUpService
      * @throws \SumUp\Exceptions\SumUpAuthenticationException
      * @throws \SumUp\Exceptions\SumUpSDKException
      */
-    public function create($amount, $currency, $checkoutRef, $payToEmail, $description = '', $payFromEmail = null, $returnURL = null, $redirectURL = null)
+    public function create($amount, $currency, $checkoutRef, $merchantCode, $description = '', $payFromEmail = null, $returnURL = null, $redirectURL = null)
     {
         if (empty($amount) || !is_numeric($amount)) {
             throw new SumUpArgumentException(ExceptionMessages::getMissingParamMsg('amount'));
@@ -72,16 +72,15 @@ class Checkouts implements SumUpService
         if (empty($checkoutRef)) {
             throw new SumUpArgumentException(ExceptionMessages::getMissingParamMsg('checkout reference id'));
         }
-        if (empty($payToEmail)) {
-            throw new SumUpArgumentException(ExceptionMessages::getMissingParamMsg('pay to email'));
-        }
+
         $payload = [
             'amount' => $amount,
             'currency' => $currency,
+            'merchant_code' => $merchantCode,
             'checkout_reference' => $checkoutRef,
-            'pay_to_email' => $payToEmail,
             'description' => $description
         ];
+
         if (isset($payFromEmail)) {
             $payload['pay_from_email'] = $payFromEmail;
         }
